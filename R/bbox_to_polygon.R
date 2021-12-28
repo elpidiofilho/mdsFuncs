a395b09553d98096f9cd7a93f61de6de7a955f75
Elpidio Filho <elpidiofilho@gmail.com>
2021-12-28 12:28:25 -0300

    bbox to polygon

---
+++ R/bbox_to_polygon.R
@@ -0,0 +1,17 @@
+#' bbox to polygon
+#' @description extract a bounding box of a vector file and convert
+#'     to a sf polygon
+#' @param vector in sf ou vect format
+#' @importFrom sf st_bbox  st_as_sfc
+#' @return sf polygin
+#' @export
+#'
+#' @examples
+#' # bbox_to_polygon(hidro)
+bbox_to_polygon <- function(vector) {
+  poly2 <- vector |>
+    st_bbox() |>
+    st_as_sfc()
+  return(poly2)
+
+}
