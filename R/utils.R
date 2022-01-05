89f50ff9b5f3abe0628c37a4b90989f85119e260
Elpidio Filho <elpidiofilho@gmail.com>
2022-01-05 17:50:28 -0300

    get epsg

--- R/utils.R
+++ R/utils.R
@@ -47,3 +47,37 @@ IsBiggerOrEqual <- function(a, b) {
 exit <- function() {
   invokeRestart("abort")
 }
+
+
+#' Get epsg SIRGAS
+#' @description get the epsg to convert geo to UTM projection and SIRGAS datum
+#' @param lonlat numeric vector of size 2 with values of longitud and latitud
+#' @return numeric epsg number
+#' @export
+#' @examples
+#' get_epsg_SIRGAS(c(-42, -20))
+get_epsg_SIRGAS = function(lonlat) {
+  utm = (floor((lonlat[1] + 180) / 6) %% 60) + 1
+  if (lonlat[2] < 0) {
+    utm + 31960
+  } else{
+    stop('Use WGS84 instead SIRGAS')
+  }
+}
+
+
+#' Get epsg WGS
+#' @description get the epsg to convert geo to UTM projection and WGS datum
+#' @param lonlat numeric vector of size 2 with values of longitud and latitud
+#' @return numeric epsg number
+#' @export
+#' @examples
+#' get_epsg_WGS(c(-42, -20))
+get_epsg_WGS = function(lonlat) {
+  utm = (floor((lonlat[1] + 180) / 6) %% 60) + 1
+  if(lonlat[2] > 0) {
+    utm + 32600
+  } else{
+    utm + 32700
+  }
+}
